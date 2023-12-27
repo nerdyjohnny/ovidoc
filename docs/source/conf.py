209d67baca4960592703b413d7f101b31996a095
nerdyjohnny <13726809+nerdyjohnny@users.noreply.github.com>
2023-12-26 22:15:54 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,8 +6,8 @@ project = 'Oiivae Community'
 copyright = '2021, nerdyjohnny and Oiivae'
 author = 'nerdyjohnny'
 
-release = '0.8'
-version = '0.8.0'
+release = '0.9'
+version = '0.9.0'
 
 # -- General configuration
 
